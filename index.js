0f6dc2cb08c2e702f9b5ad7acb5f3681003e1bdd
Favor-star <favoureliab@gmail.com>
2023-08-31 00:19:36 +0200

    FInal touch before push

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 "use strict";
 let adviceDiv = document.getElementById("advice-id");
 let titleDiv = document.getElementById("title-id");
+let changer = document.getElementById("icon");
 
 function getData() {
   return new Promise((resolve, reject) => {
     fetch("https://api.adviceslip.com/advice", { mode: "cors" })
       .then((response) => {
-        console.log("Success");
-        return response.json();f
+        // console.log("Success");
+        return response.json();
       })
       .then((info) => {
-        console.log(info);
+        // console.log(info);
         resolve(info);
       })
       .catch((error) => {
-        console.log(error);
+        alert(error);
         reject(error);
       });
   });
 }
-
-getData()
+function displayData() {
+    getData()
   .then((data) => {
       titleDiv.textContent = ` Advice  # ${data.slip.id}`
       adviceDiv.textContent = `${data.slip.advice}`;
   })
   .catch((error) => {});
+}
+window.onload = displayData();
 
+  changer.addEventListener('click', displayData)
 
